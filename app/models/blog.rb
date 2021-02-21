1911160fa176085fb3d4f4eabbe385db73824484
Akashdeep Singh Khanuja <akhanuja@gammastack.com>
2021-02-21 20:04:08 +0530

    Implemented enum option for portfolios to change the state

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,7 @@
 class Blog < ApplicationRecord
+    
+    # saves many lines of codes
+	enum status: { draft: 0, published: 1 }
 	extend FriendlyId
 	friendly_id :title, use: :slugged
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_082037) do
+ActiveRecord::Schema.define(version: 2021_02_21_142147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
-    t.text "body" 
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
