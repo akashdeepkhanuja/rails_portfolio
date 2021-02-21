49f26a2bef36aa7bf4025d8dbf6d15d490f24269
Akashdeep Singh Khanuja <akhanuja@gammastack.com>
2021-02-21 10:35:52 +0530

    added edit page and method in portfolio_controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,6 +25,29 @@ class PortfoliosController < ApplicationController
   end
 
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+
+
+    
+  end
+
+
+  def update
+  	@portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+        
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        
+      end
+    end
+
+  end
+
+
 
 end
  
