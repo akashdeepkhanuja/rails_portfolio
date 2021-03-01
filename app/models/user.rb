c3443154b03b8dd2ef5a5937d42aad306ec58b7d
Akashdeep Singh Khanuja <akhanuja@gammastack.com>
2021-03-01 13:27:35 +0530

    added req for petergate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
  class User < ApplicationRecord
+  
+
+
+
+  petergate(roles: [:admin, :editor], multiple: false)
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
