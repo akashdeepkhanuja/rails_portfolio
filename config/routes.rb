7627e1b9f892a6dd108f2382418d04390c622b7e
Akashdeep Singh Khanuja <akhanuja@gammastack.com>
2021-03-01 13:02:56 +0530

    installed bootstrap styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  
   resources :portfolios, except: [:show]  
+  
   get 'portfolio/:id', to: 'portfolios#show', as:'portfolio_show'
+  
   get 'angular-items', to: 'portfolios#angular'
+  
   root to: 'pages#home'
   
   get 'about-me', to: 'pages#about'
+  
   get 'contact', to: 'pages#contact'
   
   resources :blogs do
